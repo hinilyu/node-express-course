dcc057beba304bfc8681db748a4fda4bca7e3996
Daniel Home <hinilyu@gmail.com>
2023-07-25 16:20:01 -0400

    error handling

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,21 +1,68 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors/index");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  let jobs = await Job.find({ createdBy: req.user.userID }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-  res.send("get jobs");
+  const {
+    user: { userID },
+    params: { id: jobID },
+  } = req;
+  const job = await Job.findOne({
+    _id: jobID,
+    createdBy: userID,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobID}`);
+  }
+  res.status(StatusCodes.OK).json(job);
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userID;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send("update job");
+  const {
+    body: { company, position },
+    user: { userID },
+    params: { id: jobID },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Please provide company or position value");
+  }
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobID, createdBy: userID },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobID}`);
+  }
+  res.status(StatusCodes.OK).json(job);
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const {
+    user: { userID },
+    params: { id: jobID },
+  } = req;
+
+  const job = await Job.findOneAndRemove({ _id: jobID, createdBy: userID });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobID}`);
+  }
+
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
